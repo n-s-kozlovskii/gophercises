eef944e3dedf2c6c2df77162d1248e3bee1ab011
N K <n.s.kozlovskii@gmail.com>
2025-12-16 00:14:32 +0400

    added timer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type question struct {
@@ -36,6 +38,19 @@ func main() {
 		}
 		questions = append(questions, question{record[0], record[1]})
 	}
+	if os.Args[1] == "-limit" {
+		limit, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Printf("usage: ./quiz -limit time_limit_seconds, got %s/n", os.Args[2])
+		}
+		timer := time.NewTimer(time.Duration(limit) * time.Second)
+		go func() {
+			defer os.Exit(0)
+			<-timer.C
+			fmt.Printf("\nPassed %d seconds, you're done;\nscore is %d\n", limit, score)
+		}()
+	}
+
 	for _, q := range questions {
 		fmt.Printf("%s? ", q.question)
 		userAnswer, err := scanner.ReadString('\n')
