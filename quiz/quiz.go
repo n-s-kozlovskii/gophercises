990b58c01595cc16cb1dbfafc3efa90aa34fa2a4
N K <n.s.kozlovskii@gmail.com>
2025-12-16 23:45:37 +0400

    add flag support

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -18,13 +18,16 @@ type question struct {
 }
 
 func main() {
+	filePathFlag := flag.String("csv", "questions.csv", "path to file with questions in format 'question,answer'")
+	limit := flag.Int("limit", 30, "time limit of quiz in seconds")
+	flag.Parse()
 
 	score := 0
 	questions := make([]question, 0, 100)
 	scanner := bufio.NewReader(os.Stdin)
-	file, err := os.Open("example.csv")
+	file, err := os.Open(*filePathFlag)
 	if err != nil {
-		log.Fatalf("failed to open file: %s", err)
+		log.Fatalf("failed to open file: %s", filePathFlag)
 	}
 	defer file.Close()
 	r := csv.NewReader(file)
@@ -38,18 +41,13 @@ func main() {
 		}
 		questions = append(questions, question{record[0], record[1]})
 	}
-	if os.Args[1] == "-limit" {
-		limit, err := strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Printf("usage: ./quiz -limit time_limit_seconds, got %s/n", os.Args[2])
-		}
-		timer := time.NewTimer(time.Duration(limit) * time.Second)
-		go func() {
-			defer os.Exit(0)
-			<-timer.C
-			fmt.Printf("\nPassed %d seconds, you're done;\nscore is %d\n", limit, score)
-		}()
-	}
+
+	timer := time.NewTimer(time.Duration(*limit) * time.Second)
+	go func() {
+		defer os.Exit(0)
+		<-timer.C
+		fmt.Printf("\nPassed %d seconds, you're done;\nscore is %d\n", limit, score)
+	}()
 
 	for _, q := range questions {
 		fmt.Printf("%s? ", q.question)
